f4dd4e31429b6f87cb4cfff8cbe95877d4010d50
Guo Yunhe <guoyunhebrave@gmail.com>
2018-05-11 12:44:11 +0300

    Fetch polyphone

--- fetch-hanzidb.js
+++ fetch-hanzidb.js
@@ -8,6 +8,8 @@ const hanzi_no_tone_pinyin_table = {};
 const pinyin_hanzi_table = {};
 const no_tone_pinyin_hanzi_table = {};
 
+let hanzi_array;
+
 function fetch_hanzidb(page = 1) {
   axios
     .get("http://hanzidb.org/character-list/by-frequency", {
@@ -18,7 +20,7 @@ function fetch_hanzidb(page = 1) {
     .then(function(response) {
       console.log("Page " + page);
       const $ = cheerio.load(response.data);
-      $("table tr:not(:first-child)").each(function(index, element) {
+      $("table tr:not(:first-child)").each(function() {
         const hanzi = $(this)
           .find("td:first-child")
           .text();
@@ -27,73 +29,121 @@ function fetch_hanzidb(page = 1) {
           .find("td:nth-child(2)")
           .text();
         console.log(pinyin);
-        const no_tone_pinyin = remove_tone(pinyin);
 
-        if (!hanzi_pinyin_table[hanzi]) {
-          hanzi_pinyin_table[hanzi] = [];
-        }
-        hanzi_pinyin_table[hanzi].push(pinyin);
-
-        if (!hanzi_no_tone_pinyin_table[hanzi]) {
-          hanzi_no_tone_pinyin_table[hanzi] = [];
-        }
-        hanzi_no_tone_pinyin_table[hanzi] = _.union(
-          hanzi_no_tone_pinyin_table[hanzi],
-          [no_tone_pinyin]
-        );
-
-        if (!pinyin_hanzi_table[pinyin]) {
-          pinyin_hanzi_table[pinyin] = [];
-        }
-        pinyin_hanzi_table[pinyin].push(hanzi);
-
-        if (!no_tone_pinyin_hanzi_table[no_tone_pinyin]) {
-          no_tone_pinyin_hanzi_table[no_tone_pinyin] = [];
-        }
-        no_tone_pinyin_hanzi_table[no_tone_pinyin] = _.union(
-          no_tone_pinyin_hanzi_table[no_tone_pinyin],
-          [hanzi]
-        );
+        add_hanzi_pinyin(hanzi, pinyin);
       });
+
       fetch_hanzidb(page + 1);
     })
-    .catch(function() {
-      fs.writeFile(
-        "hanzi-pinyin-table.json",
-        JSON.stringify(hanzi_pinyin_table),
-        "utf8",
-        function(err) {
-          if (err) console.log(err);
-        }
-      );
-
-      fs.writeFile(
-        "hanzi-no-tone-pinyin-table.json",
-        JSON.stringify(hanzi_no_tone_pinyin_table),
-        "utf8",
-        function(err) {
-          if (err) console.log(err);
-        }
-      );
-
-      fs.writeFile(
-        "pinyin-hanzi-table.json",
-        JSON.stringify(pinyin_hanzi_table),
-        "utf8",
-        function(err) {
-          if (err) console.log(err);
-        }
-      );
-
-      fs.writeFile(
-        "no-tone-pinyin-hanzi-table.json",
-        JSON.stringify(no_tone_pinyin_hanzi_table),
-        "utf8",
-        function(err) {
-          if (err) console.log(err);
-        }
-      );
+    .catch(function(response) {
+      console.log(response.status);
+      hanzi_array = Object.keys(hanzi_pinyin_table);
+      fetch_polyphone();
     });
+}
+
+/**
+ * Fetch pinyin of polyphones.
+ *
+ * @param {number} index Array index of hanzi_array
+ */
+function fetch_polyphone(index = 0) {
+  if (index >= hanzi_array.length) {
+    save_json_files();
+    return;
+  }
+  const hanzi = hanzi_array[index];
+  console.log(hanzi);
+  axios
+    .get("http://hanzidb.org/character/" + encodeURIComponent(hanzi))
+    .then(function(response) {
+      const $ = cheerio.load(response.data);
+      $(".ceent > div:first-child span").each(function() {
+        const pinyin = $(this)
+          .text()
+          .toLocaleLowerCase();
+        console.log(pinyin);
+        add_hanzi_pinyin(hanzi, pinyin);
+      });
+      fetch_polyphone(index + 1);
+    })
+    .catch(function(response) {
+      console.log("Error while fetching data of " + hanzi);
+    });
+}
+
+/**
+ * Add Hanzi-Pinyin pair to dictionary
+ *
+ * @param {string} hanzi
+ * @param {string} pinyin
+ */
+function add_hanzi_pinyin(hanzi, pinyin) {
+  const no_tone_pinyin = remove_tone(pinyin);
+
+  if (!hanzi_pinyin_table[hanzi]) {
+    hanzi_pinyin_table[hanzi] = [];
+  }
+  hanzi_pinyin_table[hanzi] = _.union(hanzi_pinyin_table[hanzi], [pinyin]);
+
+  if (!hanzi_no_tone_pinyin_table[hanzi]) {
+    hanzi_no_tone_pinyin_table[hanzi] = [];
+  }
+  hanzi_no_tone_pinyin_table[hanzi] = _.union(
+    hanzi_no_tone_pinyin_table[hanzi],
+    [no_tone_pinyin]
+  );
+
+  if (!pinyin_hanzi_table[pinyin]) {
+    pinyin_hanzi_table[pinyin] = [];
+  }
+  pinyin_hanzi_table[pinyin] = _.union(pinyin_hanzi_table[pinyin], [hanzi]);
+
+  if (!no_tone_pinyin_hanzi_table[no_tone_pinyin]) {
+    no_tone_pinyin_hanzi_table[no_tone_pinyin] = [];
+  }
+  no_tone_pinyin_hanzi_table[no_tone_pinyin] = _.union(
+    no_tone_pinyin_hanzi_table[no_tone_pinyin],
+    [hanzi]
+  );
+}
+
+function save_json_files() {
+  fs.writeFile(
+    "hanzi-pinyin-table.json",
+    JSON.stringify(hanzi_pinyin_table),
+    "utf8",
+    function(err) {
+      if (err) console.log(err);
+    }
+  );
+
+  fs.writeFile(
+    "hanzi-no-tone-pinyin-table.json",
+    JSON.stringify(hanzi_no_tone_pinyin_table),
+    "utf8",
+    function(err) {
+      if (err) console.log(err);
+    }
+  );
+
+  fs.writeFile(
+    "pinyin-hanzi-table.json",
+    JSON.stringify(pinyin_hanzi_table),
+    "utf8",
+    function(err) {
+      if (err) console.log(err);
+    }
+  );
+
+  fs.writeFile(
+    "no-tone-pinyin-hanzi-table.json",
+    JSON.stringify(no_tone_pinyin_hanzi_table),
+    "utf8",
+    function(err) {
+      if (err) console.log(err);
+    }
+  );
 }
 
 /**
