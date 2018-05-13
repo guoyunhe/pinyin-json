46eb8489e8a55d75aa1bd021cd2d3e40a59d67d2
Guo Yunhe <guoyunhebrave@gmail.com>
2018-05-13 22:01:29 +0300

    Download wikipedia index

--- download-wikipedia-dump.sh
+++ download-wikipedia-dump.sh
@@ -3,3 +3,4 @@
 mkdir data
 cd data
 wget https://dumps.wikimedia.org/zhwiki/20180501/zhwiki-20180501-pages-articles-multistream.xml.bz2
+wget https://dumps.wikimedia.org/zhwiki/20180501/zhwiki-20180501-pages-articles-multistream-index.txt.bz2
